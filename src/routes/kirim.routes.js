b321f072abbde2ed0b590e6446544a46d0068f60
teduhafr <84707693+teduhafr@users.noreply.github.com>
2024-06-21 15:02:19 +0700

    add unduh csv

--- src/routes/kirim.routes.js
+++ src/routes/kirim.routes.js
@@ -1,4 +1,5 @@
 const db = require('../model');
+const { Parser } = require('json2csv');
 const dblaptop = db.laptop;
 
 module.exports = app => {
@@ -40,6 +41,28 @@ module.exports = app => {
         });
         }
     );
+
+    router.get('/unduh-csv', async (req, res) => {
+        try {
+          // Fetch data using Sequelize
+          const datajodo = await dblaptop.findAll();
+      
+          // Convert to JSON
+          const dataJson = datajodo.map(datajodo => datajodo.toJSON());
+      
+          // Convert JSON to CSV
+          const json2csvParser = new Parser();
+          const csv = json2csvParser.parse(dataJson);
+      
+          // Set headers to prompt the user to download the file
+          res.header('Content-Type', 'text/csv');
+          res.attachment('data.csv');
+          res.send(csv);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Internal Server Error');
+        }
+      });
 
     app.use('/api', router);
 };
